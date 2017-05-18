a48d08b80b6d672fde1518ff53b89afe667a8b1e
Clément Texier <ctexier@sodifrance.fr>
2017-05-18 17:32:48 +0200

    Initial commit

---
+++ js/JSPaint.js
@@ -0,0 +1,38 @@
+/**
+ * Created by ctexier on 15/05/2017
+ */
+var canvas          = document.querySelector('#drawing-area');
+var ctx             = canvas.getContext('2d');
+
+var sketch          = document.querySelector('#sketch');
+var sketch_style    = getComputedStyle(sketch);
+canvas.width        = parseInt(sketch_style.getPropertyValue('width'));
+canvas.height       = parseInt(sketch_style.getPropertyValue('height'));
+
+var mouse           = {x: 0, y: 0};
+
+canvas.addEventListener('mousemove', function(e) {
+    mouse.x = e.pageX - this.offsetLeft;
+    mouse.y = e.pageY - this.offsetTop;
+}, false);
+
+ctx.lineWidth   = 5;
+ctx.lineJoin    = 'round';
+ctx.lineCap     = 'round';
+ctx.strokeStyle = 'blue';
+
+canvas.addEventListener('mousedown', function(e) {
+    ctx.beginPath();
+    ctx.moveTo(mouse.x, mouse.y);
+
+    canvas.addEventListener('mousemove', onPaint, false);
+}, false);
+
+canvas.addEventListener('mouseup', function() {
+    canvas.removeEventListener('mousemove', onPaint, false);
+}, false);
+
+var onPaint = function() {
+    ctx.lineTo(mouse.x, mouse.y);
+    ctx.stroke();
+};
